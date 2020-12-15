8b4ffc841e05ccde9493166e4085d22bd8ee7c1a
Ben Rainir Sousa Ferreira <ben.ferreira@secult.ce.gov.br>
2020-12-15 11:28:47 -0300

    seed de usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'wylder',
+            'email' => 'benrainir@gmail.com',
+            'password' => md5('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'wylder',
+            'email' => 'mario@gmail.com',
+            'password' => md5('password'),
+        ]);
     }
 }
